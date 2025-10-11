c13ca79be01bc507bb5618fa1440aa823aaf801c
baiyin <17693374267@163.com>
2025-10-11 23:52:27 +0800

    引入junit，使用Resource代替Autowired

--- src/test/java/com/zixi/usermanagementsystem/UserMapperTest.java
+++ src/test/java/com/zixi/usermanagementsystem/UserMapperTest.java
@@ -2,24 +2,24 @@ package com.zixi.usermanagementsystem;
 
 import com.zixi.usermanagementsystem.mapper.UserMapper;
 import com.zixi.usermanagementsystem.model.User;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 import java.util.List;
 
 @SpringBootTest
 class UserMapperTest {
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
     @Test
     void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
-        Assert.isTrue(5 == userList.size(), "");
+        Assertions.assertEquals(5, userList.size(), "");
         userList.forEach(System.out::println);
     }
 
