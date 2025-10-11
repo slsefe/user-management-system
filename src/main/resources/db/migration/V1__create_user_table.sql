9cf501d2b50b655fba3741cb22c180922ca50f5e
baiyin <17693374267@163.com>
2025-10-11 23:33:02 +0800

    增加flyway配置，增加数据库脚本

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -3,13 +3,9 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
     id         BIGINT      NOT NULL auto_increment primary key,
-    first_name VARCHAR(30) NULL,
-    last_name  VARCHAR(30) NULL,
-    email      VARCHAR(50) NULL,
-    phone      VARCHAR(20) NOT NULL,
+    name       VARCHAR(30) NULL,
     age        INT         NOT NULL,
-    gender     SMALLINT    NOT NULL,
-    birthday   date        NOT NULL,
-    created_at datetime    DEFAULT CURRENT_TIMESTAMP,
-    updated_at datetime    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    email      VARCHAR(50) NULL,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
