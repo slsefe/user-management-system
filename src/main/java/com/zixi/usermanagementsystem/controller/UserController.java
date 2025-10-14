f0cffac801936ec776048c3523d730067f440d6a
baiyin <17693374267@163.com>
2025-10-14 22:34:26 +0800

    实现管理员查询和删除用户

--- src/main/java/com/zixi/usermanagementsystem/controller/UserController.java
+++ src/main/java/com/zixi/usermanagementsystem/controller/UserController.java
@@ -7,10 +7,15 @@ import com.zixi.usermanagementsystem.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,5 +32,15 @@ public class UserController {
     @PostMapping("/login")
     public User login(@RequestBody @Valid UserLoginRequest userLoginRequest, HttpServletRequest request) {
         return userService.login(userLoginRequest, request);
+    }
+
+    @GetMapping
+    public List<User> query() {
+        return userService.list();
+    }
+
+    @DeleteMapping("/{userId}")
+    public Boolean delete(@PathVariable Long userId) {
+        return userService.removeById(userId);
     }
 }
