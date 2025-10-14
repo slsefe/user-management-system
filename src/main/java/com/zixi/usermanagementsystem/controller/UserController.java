9e01252e4c4c85077914b21dd5a86ee3abe66605
baiyin <17693374267@163.com>
2025-10-14 16:54:05 +0800

    引入springboot-validation包，启用参数校验

--- src/main/java/com/zixi/usermanagementsystem/controller/UserController.java
+++ src/main/java/com/zixi/usermanagementsystem/controller/UserController.java
@@ -2,6 +2,7 @@ package com.zixi.usermanagementsystem.controller;
 
 import com.zixi.usermanagementsystem.controller.dto.UserRegisterDTO;
 import com.zixi.usermanagementsystem.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,15 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/register")
-    public Long register(@RequestBody UserRegisterDTO userRegisterDTO) {
-        // TODO: 增加参数校验
+    public Long register(@RequestBody @Valid UserRegisterDTO userRegisterDTO) {
         return userService.register(userRegisterDTO);
     }
 }
