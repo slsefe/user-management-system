8f9e4cb5405bc2a2c0eab7ace058599ae2ee4ae2
baiyin <17693374267@163.com>
2025-10-24 11:37:30 +0800

    支持前后端跨域

--- src/main/java/com/zixi/usermanagementsystem/controller/UserController.java
+++ src/main/java/com/zixi/usermanagementsystem/controller/UserController.java
@@ -10,6 +10,7 @@ import com.zixi.usermanagementsystem.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
 public class UserController {
 
     private final UserService userService;
