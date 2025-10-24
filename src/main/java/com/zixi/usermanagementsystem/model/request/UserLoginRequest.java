0948f34d335cc062e24bd39a599240d7c8ae759a
baiyin <17693374267@163.com>
2025-10-25 00:31:38 +0800

    移除用户注册和登录的正则校验

--- src/main/java/com/zixi/usermanagementsystem/model/request/UserLoginRequest.java
+++ src/main/java/com/zixi/usermanagementsystem/model/request/UserLoginRequest.java
@@ -1,7 +1,6 @@
 package com.zixi.usermanagementsystem.model.request;
 
 
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,10 +16,8 @@ public class UserLoginRequest implements Serializable {
     private static final long serialVersionUID = 5561561780147082745L;
 
     @Size(min = 6, max = 20, message = "账户名称长度必须在6到20位之间")
-    @Pattern(regexp = "[A-Za-z0-9_]+", message = "账户名称只能包含大小写字母、数字和下划线")
     private String account;
 
     @Size(min = 8, max = 30, message = "密码长度必须在8到30位之间")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).+$")
     private String password;
 }
--- src/main/java/com/zixi/usermanagementsystem/model/request/UserRegisterRequest.java
+++ src/main/java/com/zixi/usermanagementsystem/model/request/UserRegisterRequest.java
@@ -2,7 +2,6 @@ package com.zixi.usermanagementsystem.model.request;
 
 
 import com.zixi.usermanagementsystem.validator.PasswordValid;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,11 +18,9 @@ public class UserRegisterRequest implements Serializable {
     private static final long serialVersionUID = -3336817855294136302L;
 
     @Size(min = 6, max = 20, message = "账户名称长度必须在6到20位之间")
-    @Pattern(regexp = "[A-Za-z0-9_]+", message = "账户名称只能包含大小写字母、数字和下划线")
     private String account;
 
     @Size(min = 8, max = 30, message = "密码长度必须在8到30位之间")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).+$")
     private String password;
 
     private String checkPassword;
